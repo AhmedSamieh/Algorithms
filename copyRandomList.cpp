4c5a14cb43387d9d84ad1e993064b9e8b925f4ba
Ahmed Samieh <ahmed.samieh@gmail.com>
2019-11-16 02:43:42 -0700

    new algorithm

---
+++ copyRandomList.cpp
@@ -0,0 +1,39 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+
+    Node() {}
+
+    Node(int _val, Node* _next, Node* _random) {
+        val = _val;
+        next = _next;
+        random = _random;
+    }
+};
+*/
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        unordered_map<Node*, Node*> m; // key=oldPointer, val=newPointer
+        m.insert(make_pair<Node*, Node*>(NULL, NULL));
+        for (Node* scanner = head; scanner != NULL; scanner = scanner->next)
+        {
+            Node* node = new Node();
+            m.insert(make_pair(scanner, node));
+        }
+        for (auto i : m)
+        {
+            if (i.first != NULL)
+            {
+                i.second->val = i.first->val;
+                i.second->next = m[i.first->next];
+                i.second->random = m[i.first->random];
+            }
+        }
+        return m[head];
+    }
+};
