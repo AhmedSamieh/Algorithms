ade6bb0b7b80871d9f9a8121437b81c1fcc8a361
Ahmed Samieh <ahmed.samieh@gmail.com>
2019-11-19 10:07:50 -0700

    add more algorithms

---
+++ removeOuterParentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string removeOuterParentheses(string S) {
+        string s;
+        int step = 0;
+        for (auto c : S)
+        {
+            if (c == '(')
+            {
+                if (++step == 1)
+                {
+                    continue;
+                }
+            }
+            else if (c == ')')
+            {
+                if (step-- == 1)
+                {
+                    continue;
+                }
+            }
+            s.push_back(c);
+        }
+        return s;
+    }
+};
---
+++ uniqueMorseRepresentations.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int uniqueMorseRepresentations(vector<string>& words) {
+        const string code_map[] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        unordered_set<string> s;
+        for (auto const &word : words)
+        {
+            string code;
+            for (auto c : word)
+            {
+                code += code_map[int(c) - int('a')];
+            }
+            s.insert(code);
+        }
+        return s.size();
+    }
+};
